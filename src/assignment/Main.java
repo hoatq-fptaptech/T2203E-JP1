f6c28768a15b6f7ef44b23fb652bc45c2d05ba2b
billytrinh <quanghoa.trinh@gmail.com>
2022-09-20 18:34:28 +0700

    asm 2

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -4,6 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Account myAccount = new Account(1,"Trịnh Quang Hòa",1000000);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("NHap so tien muon rut:");
+        int x = sc.nextInt();
+        if(myAccount.debit(x)){
+            System.out.println("Rut thanh cong "+x);
+        }else{
+            System.out.println("So du tai khoan khong du hoac nhap sai so tien");
+        }
+
+        Account nam = new Account(2,"Cao Hoàng Nam",10000);
+        if(myAccount.tranferTo(nam,10000)){
+            System.out.println("Chuyen thanh cong sang tai khoan "+nam.getName());
+        }else{
+            System.out.println("Chuyen khoan that bai");
+        }
+    }
+
+    public static void mainOld(String[] args){
         Scanner sc = new Scanner(System.in);
         //int x = sc.nextInt();// <=> scanf("%d",&x);
         PhanSo f1 = new PhanSo();
