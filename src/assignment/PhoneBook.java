417d1109f782b06a599a2fa005c61999f2cf014a
billytrinh <quanghoa.trinh@gmail.com>
2022-09-22 21:11:56 +0700

    goi y asm

---
+++ src/assignment/PhoneBook.java
@@ -0,0 +1,7 @@
+package assignment;
+
+import java.util.ArrayList;
+
+public class PhoneBook extends Phone{
+    public ArrayList<PhoneNumber> PhoneList;
+}
---
+++ src/assignment/PhoneNumber.java
@@ -0,0 +1,8 @@
+package assignment;
+
+import java.util.ArrayList;
+
+public class PhoneNumber {
+    String name;
+    ArrayList<String> phone;
+}
